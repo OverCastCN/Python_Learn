f59f8f0d8cee5dc1b1721c6ecc59f16e281da9fd
OverCast <1101066076@qq.com>
2017-07-17 08:28:36 +0800

    新增了字符串修饰笔记和示例代码

---
+++ OverCast/python基础——常用目录、字符串/src/字符串的修饰.py
@@ -0,0 +1,38 @@
+print "Center:"
+print "Center".center(10)
+print "Center".center(10,"*")
+print "okr".center(10,"*")
+print ""
+
+print "ljust:"
+print "ljust".ljust(10)
+print "ljust".ljust(10,"*")
+print ""
+
+print "rjust:"
+print "rjust".rjust(10)
+print "rjust".rjust(10,"*")
+
+print ""
+
+print "zfill:"
+print "zfill".zfill(10)
+print ""
+
+print "format:"
+print "{} is always {} years old.".format("OverCast",18)
+print ""
+
+print "strip:"
+print "  strip  ".strip()
+print "**strip**".strip("*")
+print ""
+
+print "lstrip:"
+print "  lstrip  ".lstrip()
+print "**lstrip**".lstrip("*")
+print ""
+
+print "rstrip:"
+print "  rstrip  ".rstrip()
+print "**rstrip**".rstrip("*")
