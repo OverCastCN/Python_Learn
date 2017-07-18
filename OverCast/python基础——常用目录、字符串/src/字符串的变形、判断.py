09c641a9a09a27c427b59fa9a22aa96aa99dc074
OverCast <1101066076@qq.com>
2017-07-18 22:33:18 +0800

    新郑字符串的变形和判断代码和笔记

---
+++ OverCast/python基础——常用目录、字符串/src/字符串的变形、判断.py
@@ -0,0 +1,54 @@
+print "I am student".upper()
+print "I am student".lower()
+print "I am student".swapcase()
+print "I am student".title()
+print "I am student\tyou are my teacher"
+print "I am student\tyou are my teacher".expandtabs(1)
+print "i am student\tyou are my teacher".capitalize()
+print
+
+print "isalnum:"
+print "Computer1".isalnum()
+print "Computer1 ".isalnum()
+print "Computer1@".isalnum()
+print
+
+print "isalpha:"
+print "Computer".isalpha()
+print "Computer1".isalpha()
+print "Computer!@".isalpha()
+print
+
+print "isdigit:"
+print "123546987".isdigit()
+print "12@3sdwwex7".isdigit()
+print "12@7".isdigit()
+print
+
+print "isupper:"
+print "COMPUTER".isupper()
+print "COMPUTEr".isupper()
+print
+
+print "islower:"
+print "computer".islower()
+print "Computer".islower()
+print
+
+print "spaces:"
+print "     ".isspace()
+print "   \t  ".isspace()
+print "   a  ".isspace()
+
+print "I Am A Student".istitle()
+print "I am a student".istitle()
+print
+
+print "startwith:"
+print "HelloWorld".startswith("H")
+print "HelloWorld".startswith("Hello")
+print
+
+print "endswith:"
+print "HelloWorld".endswith("d")
+print "HelloWorld".endswith("World")
