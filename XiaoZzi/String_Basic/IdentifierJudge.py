3f2fe1e38d7479ae38757dd62f3e5cb137d0e98d
XiaoZzi <980850796@qq.com>
2017-07-19 11:14:42 +0800

    练习题，输入一系列字符串每个占一行，判断是否为合法标识符并输出

--- XiaoZzi/String_Basic/IdentifierJudge.py
+++ XiaoZzi/String_Basic/IdentifierJudge.py
@@ -1,29 +1,25 @@
 # -*- coding:utf-8 -*-
 """
 判断一个字符串是否为合法python标识符
+输入格式:
+一系列字符串，每个字符串占一行。
+输出格式：
+判断每行字符串是否为合法的 Python 标示符，如果合法则输出 True，否则输出 False。
 """
 
 import keyword
 
 def judge(s):
-
-    if s[0] == '_' or ord(s[0]) in (range(ord('a'),ord('z')+1)  or range(ord('A'),ord('Z')+1)):
-        for i in range(len(s)):
-            if s[i] != '_' and s[i] not in (range(ord('a'), ord('z') + 1)) or range(ord('A'), ord('Z') + 1) and type(
-                    s[i]) != int:
-                return False
-
-    elif keyword.iskeyword(s):
+    if keyword.iskeyword(s):
         return False
-
-    else:
+    elif s[0] == '_' or ord(s[0]) in (range(ord('a'),ord('z')+1) or range(ord('A'),ord('Z')+1)):
+        for i in range(len(s)):
+            if s[i] != '_' and ord(s[i]) not in (range(ord('a'), ord('z') + 1) or range(ord('A'), ord('Z') + 1)) and type(
+                            s[i]) != int:
+                 return False
         return True
-
-s = 'od'
-if s[0] != '_' and ord(s[0]) not in (range(ord('a'), ord('z') + 1) or range(ord('A'), ord('Z') + 1)):
-    print False
-else:
-    print True
+    else:
+        return False
 
 stopwoed = ''
 str = ''
