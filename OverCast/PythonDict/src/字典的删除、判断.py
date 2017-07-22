077337621787b7e4f223367d9750f28883942628
OverCast <1101066076@qq.com>
2017-07-23 07:28:23 +0800

    新增了字典的删除和判断、字典的视图和迭代的代码和笔记

---
+++ OverCast/PythonDict/src/字典的删除、判断.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+
+# 字典的删除
+# ourDict = dict(zip("hello","12345"))
+# print ourDict
+# print ourDict.pop("h")
+#
+# print ourDict.popitem()
+# print ourDict.popitem()
+# print ourDict.popitem()
+#
+# ourDict.clear()
+# print  ourDict
+
+# 字典的判断
+ourDict = dict(zip("hello","12345"))
+
+print ourDict.has_key("h")
+print ourDict
+print ourDict.has_key("L")
+print "2" in ourDict
