f0d0a93869b15d364ddd37c714bc95a8ad667dfd
XiaoZzi <980850796@qq.com>
2017-07-24 10:16:15 +0800

    文件操作

--- XiaoZzi/FileManipulation_Basic/OpenMode.py
+++ XiaoZzi/FileManipulation_Basic/OpenMode.py
@@ -1,6 +1,7 @@
 # coding:utf-8
-f = open('F:\PythonLearm\Python_Learn\XiaoZzi\FileManipulation_Basic\\PracticeFile','r+')
+f = open('D:\\My Study\\Python\\XiaoZzi\\Python_Learn\\XiaoZzi\\FileManipulation_Basic\\PracticeFile','a')
+# f.write('q')
+# f.flush()
+# print f.read()
 f.write('q')
-f.flush()
-print f.read()
 f.close()
