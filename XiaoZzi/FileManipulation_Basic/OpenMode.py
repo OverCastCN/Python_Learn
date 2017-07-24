c125530ee04145e2ee9e4626dd92eb75c140030f
XiaoZzi <980850796@qq.com>
2017-07-24 23:51:46 +0800

    练习题

--- XiaoZzi/FileManipulation_Basic/OpenMode.py
+++ XiaoZzi/FileManipulation_Basic/OpenMode.py
@@ -1,7 +1,24 @@
 # coding:utf-8
-f = open('D:\\My Study\\Python\\XiaoZzi\\Python_Learn\\XiaoZzi\\FileManipulation_Basic\\PracticeFile','a')
-# f.write('q')
+import io
+f = open('PracticeFile','w')
+f.write('q')
 # f.flush()
 # print f.read()
-f.write('q')
+# f.write('q')
+# list = f.readlines()
+# print list
 f.close()
+
+print io.DEFAULT_BUFFER_SIZE
+
+f2 = open('PracticeFile')
+iter_f = iter(f2)
+lines = 0
+for line in iter_f:
+    lines += 1
+print lines
+
+list_f = []
+for i in range(10):
+    list_f[i] = open('PracticeFile','r')
+    print '%d:%d' % (i,list_f[i].fileno())
