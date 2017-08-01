990a6bc8c9f39060b0e22607c220980054d2aed5
XiaoZzi <980850796@qq.com>
2017-08-01 09:13:10 +0800

    更新

--- XiaoZzi/FileManipulation_Basic/Rmd5.py
+++ XiaoZzi/FileManipulation_Basic/Rmd5.py
@@ -6,37 +6,22 @@ import hashlib
 import os
 import shutil
 
-l = []
-def getdir_md5(dirpath,md5path):  #目录地址，MD5文件地址，拷贝文件到哪个地址
-    for d_file in os.listdir(dirpath):
-        d_file = os.path.join(dirpath,d_file)
+def getdir_md5(cf, md5,ct):  #目录地址，MD5文件地址，拷贝文件到ct地址
+    for d_file in os.listdir(cf):
+        d_file = os.path.join(cf, d_file)
         if os.path.isfile(d_file):
             if os.path.splitext(d_file)[1] != '.md5':
                 m = open(d_file,'rb')
                 m1 = hashlib.md5()
                 m1.update(m.read())
-                h = m1.hexdigest()   #求出目录地址里文件的MD5值
-                # print 'strh',str(h)
-                for i in getmd5_list(md5path): #遍历给出的md5的列表
-                    # print 'i',i
+                h = m1.hexdigest()   #求cf目录里文件md5的值
+                for i in getmd5_list(md5): #遍历给出的md5的列表
                     if str(h) == i:  #判断与求出的目录中的文件的md5值是否相等
-                        print 'equal',i
-                        print d_file
-                        l.append(d_file)
-                        print len(l)
-                        break
-                        # shutil.copy(d_file,targetpath)   #如果相等，copy到targetpath
-                        # print str(h)
-                        # print 'd_file:',d_file
-                        # print 'targetpath',targetpath
-                        # pass
+                        shutil.copy(d_file,ct)   #如果相等，copy到targetpath
                 m.close()
 
         if os.path.isdir(d_file):
-            getdir_md5(d_file,md5path)   #如果是子目录，需要递归
-    # print l
-
-    return l
+            getdir_md5(d_file, md5,ct)   #如果是子目录，需要递归
 
 def getmd5_list(md5path):   #得到md5文件中的md5,返回MD5组成的列表
     lst = []
@@ -45,16 +30,12 @@ def getmd5_list(md5path):   #得到md5文件中的md5,返回MD5组成的列表
     for i in range(len(a)):
         lst.append(a[i].strip())
     f.close()
-    # print lst
     return lst
 
 
-a = u'D:\\My Work\\直播测试'
-b = 'Rmd5.txt'
-c = u'D:\\My Work\\直播测试\\md5'
+copy_from = u'D:\\My Work\\直播测试'
+md5_id = 'Rmd5.txt'
+copy_to = u'D:\\My Work\\md5'
 
-getdir_md5(a,b)
-for i in getdir_md5(a,b):
-    # shutil.copy(i,c)
-    pass
+getdir_md5(copy_from,md5_id,copy_to)
 
